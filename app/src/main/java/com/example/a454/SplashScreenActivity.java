b55681dbdfdc4defb1932205e7c8f8c06087b7d6
kmascara <kmascara@syr.edu>
2023-02-26 21:50:55 -0500

    Splash screen

--- app/src/main/java/com/example/a454/SplashScreenActivity.java
+++ app/src/main/java/com/example/a454/SplashScreenActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 //splash screen
+//Instead of theme to display we could add as an activity with an animation
+//This would be after the loading of the app
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
