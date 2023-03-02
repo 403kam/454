ccce52365257d01114476a572013af0c91799dd4
yoonjae0402 <71293632+yoonjae0402@users.noreply.github.com>
2023-03-02 15:32:11 -0500

    Add layouts

---
+++ app/src/main/java/com/example/a454/multiGameplay.java
@@ -0,0 +1,14 @@
+package com.example.a454;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class multiGameplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_gameplay);
+    }
+}
