3a53633b4c7ed0e494d41c1d402ff983f6cf83a2
yoonjae0402 <71293632+yoonjae0402@users.noreply.github.com>
2023-03-02 15:32:11 -0500

    Add layouts

---
+++ app/src/main/java/com/example/a454/singleGameplay.java
@@ -0,0 +1,14 @@
+package com.example.a454;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class singleGameplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_gameplay);
+    }
+}
